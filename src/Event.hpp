bac9660a9c1a4348222ce8d646c4762afab1c191
Timothy Sherstobitov Sergeyevich <woodgoldfilm@gmail.com>
2019-12-04 01:27:46 +0500

    добавил коментарии

--- src/Event.hpp
+++ src/Event.hpp
@@ -1,45 +1,46 @@
+//Коды клавиш клавиатуры
 #define ESC 27
 #define BACKSPACE 8
 #define SPACE 32
 #define ENTER 13
 #define SHIFT 16
+//Код минимального/максимального возможного для отображения символа
 #define MIN_CHAR 32
 #define MAX_CHAR 255
 #ifndef EVENT_HPP
 #define EVENT_HPP
 
-//РРЅС‚РµСЂС„РµР№СЃ РјРµС‚РєР° РґР»СЏ СЃС‚СѓРєС‚СѓСЂ РґР°РЅРЅС‹С… СЃРѕР±С‹С‚РёР№
-
+//Интерфейс метка для стуктур данных событий
 class Event {
-   public:
-      virtual ~Event(){};
+public:
+   virtual ~Event() {};
 };
 
+//Событие мыши
 class MouseEvent : public Event {
 public:
-    enum Type {
-        MOVE, DRAG, LEFT_KEY_DOWN, LEFT_KEY_UP
-    };
+   enum Type {
+      MOVE, DRAG, LEFT_KEY_DOWN, LEFT_KEY_UP
+   };
 
-    MouseEvent(int, int, Type, int = 0, int = 0);
-    int getX() const;
-    int getY() const;
-    int getPreX() const;
-    int getPreY() const;
-    Type getType() const;
+   MouseEvent(int, int, Type, int = 0, int = 0);//Конструктор
+   int getX() const;//Возращает позицию x нажатия мыши
+   int getY() const;// Возращает позицию y нажатия мыши
+   int getPreX() const;// Возращает предыдущую позицию x нажатия мыши
+   int getPreY() const;// Возращает предыдущую позицию y нажатия мыши
+   Type getType() const;// Возращает тип события мыши
 private:
-    int x, y, preX, preY;
-    Type type;
+   int x, y, preX, preY;//Текущие x,y, предыдущие x,y
+   Type type;//тип события мыши
 };
 
 class KeyboardEvent : public Event {
 public:
-    KeyboardEvent(int f, int fs = 0);
-    int getKeyCode() const;
-    int getPreKeyCode() const;
+   KeyboardEvent(int, int = 0);//Конструктор
+   int getKeyCode() const;//Возращает код тукущей нажатой клавиши
+   int getPreKeyCode() const;//Возращает предыдущей нажатой клавишу
 private:
-    int keyCode, preKeyCode;
+   int keyCode, preKeyCode;//Код клавиши, код предыдущей нажатой клавиши
 };
-
 #endif
 
--- src/Listener.hpp
+++ src/Listener.hpp
@@ -4,43 +4,43 @@
 
 class Listener {
 public:
-    virtual ~Listener(){}
-    virtual void onEvent(Event *event){}
+    virtual ~Listener(){}//Деструктор
+    virtual void onEvent(Event *event){}//Действие при событии
 };
 
 class OnClickListener : public Listener {
 protected:
-    virtual void onClick(MouseEvent *event){}
+    virtual void onClick(MouseEvent *event){};//Действие при клике мышкой
 private:
-    void onEvent(Event *event);
+    void onEvent(Event *event);//Проверка было ли событие кликом мышки
 };
 
 class OnPressListener : public Listener {
 protected:
-    virtual void onPress(MouseEvent *event){};
+    virtual void onPress(MouseEvent *event){};// Действие при зажатии кнопки мыши
 private:
-    void onEvent(Event *event);
+    void onEvent(Event *event);// Проверка было ли событие зажатием кнопки мыши
 };
 
 class OnDragListener : public Listener {
 protected:
-    virtual void onDrag(MouseEvent *event){};
+    virtual void onDrag(MouseEvent *event){};// Действие при перетаскивании зажатой мыши
 private:
-    void onEvent(Event *event);
+    void onEvent(Event *event);// Проверка было ли событие перетаскиванием зажатой мыши
 };
 
 class OnMoveListener : public Listener {
 protected:
-    virtual void onMove(MouseEvent *event){};
+    virtual void onMove(MouseEvent *event){};// Действие при передвижении мыши
 private:
-    void onEvent(Event *event);
+    void onEvent(Event *event);// Проверка было ли событие передвижением мыши
 };
 
 class OnKeyUpListener : public Listener {
 protected:
-    virtual void onKeyUp(KeyboardEvent *event){};
+    virtual void onKeyUp(KeyboardEvent *event){};// Действие при событии клавиатуры
 private:
-    void onEvent(Event *event);
+    void onEvent(Event *event);//Проверка является ли событие событием клавиатуры
 };
 
 #endif
